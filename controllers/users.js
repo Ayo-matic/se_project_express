2b4f934607bd792e14b764934f04c335c4e148db
Ayomikun <ayomatic002@gmail.com>
2026-07-25 05:01:48 -0500

    Validate email and password presence in login controller

--- controllers/users.js
+++ controllers/users.js
@@ -48,6 +48,12 @@ const createUser = (req, res) => {
 const login = (req, res) => {
   const { email, password } = req.body;
 
+  if (!email || !password) {
+    return res
+      .status(BAD_REQUEST)
+      .send({ message: "The email and password fields are required" });
+  }
+
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
